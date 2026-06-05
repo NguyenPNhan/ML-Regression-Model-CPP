b587f3101e22d8de3ba9789f42e73f2eaed359cf
NguyenPNhan <nguyenphunhan1110@gmail.com>
2026-06-05 18:48:06 +0700

    test gitignore

--- utils/metrics.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <vector>
-
-long double mean_absolute_error(const std::vector<long double> y_true, const std::vector<long double> y_pred);
-long double mean_squared_error(const std::vector<long double> y_true, const std::vector<long double> y_pred);
-long double r2_score(const std::vector<long double> y_true, const std::vector<long double> y_pred);
